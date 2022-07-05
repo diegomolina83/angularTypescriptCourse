0de6a6741d299a12fa8ddbb6fdd7b095bd68cd37
diegomolina83 <diegomolina83@gmail.com>
2022-07-05 21:34:29 +0200

    Typescript/Angular course

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,15 @@
+
+
+console.log('Hola Mundo!!!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Sandra";
+let hp: number | string = 100; // Para asginar valores de tipo number o tipo string indistintamente.
+let isAlive: boolean = true;
+
+nombre = "Diego";
+
+console.log(nombre, hp);
